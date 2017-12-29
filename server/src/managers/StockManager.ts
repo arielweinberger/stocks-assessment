1d7bcadf80929e1ceb23dc7fead30a08a6666afa
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2017-12-29 22:17:33 +0100

    Move mockStocks array to a separate file under the "shared" folder and use it to load the stocks.

--- server/src/managers/StockManager.ts
+++ server/src/managers/StockManager.ts
@@ -1,19 +1,12 @@
 import { Stock } from '@app/factory';
+import { mockStocks } from '@app/shared/mockStocks';
 import * as logger from 'winston';
 
 class StockManager {
-    private stocks: Map<string, Stock> = new Map();
+    private stocks: Map<number, Stock> = new Map();
 
     public loadMockStocks (): void {
         logger.info('Loading mock stocks...');
-
-        const mockStocks: { name: string, uniqueSymbol: string }[] = [
-            { name: 'Facebook', uniqueSymbol: 'FB' },
-            { name: 'Twitter', uniqueSymbol: 'TWTR' },
-            { name: 'Google', uniqueSymbol: 'GOOGL' },
-            { name: 'Amazon', uniqueSymbol: 'AMZN' },
-            { name: 'Microsoft', uniqueSymbol: 'MSFT' }
-        ];
 
         mockStocks.forEach((stock: { name: string, uniqueSymbol: string }, idx: number) => {
             this.stocks.set(stock.name, new Stock(idx, stock.name, stock.uniqueSymbol));
---
+++ server/src/shared/mockStocks.ts
@@ -0,0 +1,7 @@
+export const mockStocks: { name: string, uniqueSymbol: string }[] = [
+    { name: 'Facebook', uniqueSymbol: 'FB' },
+    { name: 'Twitter', uniqueSymbol: 'TWTR' },
+    { name: 'Google', uniqueSymbol: 'GOOGL' },
+    { name: 'Amazon', uniqueSymbol: 'AMZN' },
+    { name: 'Microsoft', uniqueSymbol: 'MSFT' }
+];
