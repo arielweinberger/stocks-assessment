64e4e7c59cdde2afc69ec9d5ade3e1a94f9a653f
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2017-12-31 00:35:30 +0100

    Install and set up Angular Material, Material Icons, create a shared MaterialModule and set style files to .scss

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { MaterialModule } from './shared/Material.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
