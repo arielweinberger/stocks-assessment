dc0445b156f8b63a7a635963a7d1476bfe8454aa
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2018-01-01 22:20:37 +0100

    Apply error handling for status code 422 - validation errors. Errors appear as Angular Meterial errors underneath the fields, dynamically.

--- client/src/app/stocks/stock-create/stock-create.component.ts
+++ client/src/app/stocks/stock-create/stock-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StockService } from '../stock.service';
 import { FeedbackService } from '../../shared/feedback.service';
@@ -11,18 +11,17 @@ import { FeedbackService } from '../../shared/feedback.service';
 })
 export class StockCreateComponent implements OnInit {
     public form: FormGroup;
+    public name: FormControl;
+    public uniqueSymbol: FormControl;
+    public price: FormControl;
 
     constructor (private router: Router,
                  private stockService: StockService,
                  private feedbackService: FeedbackService) {
+        this.createForm();
     }
 
     ngOnInit () {
-        this.form = new FormGroup({
-            name: new FormControl(),
-            uniqueSymbol: new FormControl(),
-            price: new FormControl()
-        });
     }
 
     public goToStockList () {
@@ -30,11 +29,32 @@ export class StockCreateComponent implements OnInit {
     }
 
     public onSubmit () {
-        const stock = this.form.value;
-        this.stockService.createStock(stock.name, stock.uniqueSymbol, stock.price)
+        this.stockService.createStock(this.name.value, this.uniqueSymbol.value, this.price.value)
             .subscribe(() => {
-                this.feedbackService.send(`Stock "${stock.name}" has been created!`)
+                this.feedbackService.send(`Stock "${this.name.value}" has been created!`)
                 this.goToStockList();
-            });
+            }, (error) => this.processErrors(error));
+    }
+
+    private createForm () {
+        this.name = new FormControl('', Validators.required);
+        this.uniqueSymbol = new FormControl('', Validators.required);
+        this.price = new FormControl('', Validators.required);
+
+        this.form = new FormGroup({ name: this.name, uniqueSymbol: this.uniqueSymbol, price: this.price });
+    }
+
+    private processErrors (error) {
+        const data = error.error;
+
+        /*
+         *  When validation errors occur, the server returns error code 422 (processable entity).
+         *  We will use the error code to determine what to do with the errors.
+         */
+        if (error.status === 422) {
+            Object.entries(data).forEach(([field, errorData]) => this.form.controls[field].setErrors({ backend: errorData.msg }));
+        } else {
+            console.log(error);
+        }
     }
 }
