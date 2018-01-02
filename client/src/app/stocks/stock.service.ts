3a89f331091ab941d234443abbfd3e65f969e4b4
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2018-01-02 02:27:48 +0100

    StockService: add "getStock" method to retrieve one single stock, add "updateStockPrice" method that updates a stock's price (PUT) and emits to subscribers

--- client/src/app/stocks/stock.service.ts
+++ client/src/app/stocks/stock.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/publish';
 
 @Injectable()
 export class StockService {
+    public stockUpdate: Subject<number> = new Subject();
     private readonly URL = 'http://localhost:3000/api/stocks';
 
     constructor (private httpClient: HttpClient) {
     }
 
-    getAllStocks () {
+    public getStock (stockId: number): Observable<Stock> {
+        return this.httpClient.get<Stock>(`${this.URL}/${stockId}`);
+    }
+
+    public getAllStocks (): Observable<Stock[]> {
         return this.httpClient.get<Stock[]>(this.URL);
     }
 
-    createStock (name: string, uniqueSymbol: string, price: string) {
+    public createStock (name: string, uniqueSymbol: string, price: string): Observable<{}> {
         return this.httpClient.post(this.URL, { name, uniqueSymbol, price });
+    }
+
+    public updateStockPrice (stockId: number, price: number): Observable<{}> {
+        return this.httpClient.put(`${this.URL}/${stockId}`, { price })
+            .do(() => this.stockUpdate.next(stockId));
     }
 }
