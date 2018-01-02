ccc1351901a49bb79f055b0903efed3623103a8e
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2018-01-03 00:00:36 +0100

    Add documentation for StockService

--- client/src/app/stocks/stock.service.ts
+++ client/src/app/stocks/stock.service.ts
@@ -12,18 +12,40 @@ export class StockService {
     constructor (private httpClient: HttpClient) {
     }
 
+    /**
+     * Get a single stock.
+     * @param {number} stockId
+     * @returns {Observable<Stock>}
+     */
     public getStock (stockId: number): Observable<Stock> {
         return this.httpClient.get<Stock>(`${this.URL}/${stockId}`);
     }
 
+    /**
+     * Get all stocks.
+     * @returns {Observable<Stock[]>}
+     */
     public getAllStocks (): Observable<Stock[]> {
         return this.httpClient.get<Stock[]>(this.URL);
     }
 
+    /**
+     * Create a new stock.
+     * @param {string} name
+     * @param {string} uniqueSymbol
+     * @param {string} price
+     * @returns {Observable<{}>}
+     */
     public createStock (name: string, uniqueSymbol: string, price: string): Observable<{}> {
         return this.httpClient.post(this.URL, { name, uniqueSymbol, price });
     }
 
+    /**
+     * Update a stock's price and notify subscribers to stock price changes.
+     * @param {number} stockId
+     * @param {number} price
+     * @returns {Observable<{}>}
+     */
     public updateStockPrice (stockId: number, price: number): Observable<{}> {
         return this.httpClient.put(`${this.URL}/${stockId}`, { price })
             .do(() => this.stockUpdate.next(stockId));
