c78b44788e8ddfb1652522b579ab4218ad371985
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2018-01-03 05:02:19 +0100

    Add unit tests for updateStockPrice handler

--- server/src/spec/api/handlers/stocks/getAllStocks.spec.ts
+++ server/src/spec/api/handlers/stocks/getAllStocks.spec.ts
@@ -13,18 +13,21 @@ const handler: Function = getAllStocks.handler;
 describe('getAllStocks.handler', () => {
     let stocks: Stock[];
     let res: MockResponse;
+    let statusSpy: Spy;
     let jsonSpy: Spy;
 
     beforeEach(() => {
         StockManager.loadMockStocks();
         stocks = StockManager.getAllStocks();
         res = new MockResponse();
+        statusSpy = spyOn(res, 'status').and.callThrough();
         jsonSpy = spyOn(res, 'json').and.callThrough();
     });
 
     it('returns all stocks', () => {
         handler({ params: {} }, res);
 
-        expect(jsonSpy).toHaveBeenCalledWith(stocks);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(stocks);
     });
 });
--- server/src/spec/api/handlers/stocks/updateStockPrice.spec.ts
+++ server/src/spec/api/handlers/stocks/updateStockPrice.spec.ts
@@ -1,14 +1,14 @@
 /**
- * @file getOneStock handler tests
+ * @file updateStockPrice handler tests
  * @author Ariel Weinberger
  */
-import getOneStock from '@app/api/handlers/stocks/getOneStock';
+import updateStockPrice from '@app/api/handlers/stocks/updateStockPrice';
 import { Stock } from '@app/factory';
 import StockManager from '@app/managers/StockManager';
 import { MockResponse } from '@spec/api/handlers/MockResponse';
 import Spy = jasmine.Spy;
 
-const handler: Function = getOneStock.handler;
+const handler: Function = updateStockPrice.handler;
 
 describe('getAllStocks.handler', () => {
     let stocks: Stock[];
@@ -24,16 +24,17 @@ describe('getAllStocks.handler', () => {
         jsonSpy = spyOn(res, 'json').and.callThrough();
     });
 
-    it('returns one stock if valid ID is provided', () => {
-        const targetStock: Stock = StockManager.stocks.get(1);
-        handler({ params: { id: 1 } }, res);
-
+    it('updates stock price', () => {
+        const stock: Stock = StockManager.getStockById(1);
+        stock.price = 10;
+        handler({ params: { id: 1 }, body: { price: 2.24 } }, res);
+        expect(stock.price).toEqual(2.24);
         expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith(targetStock);
+        expect(res.json).toHaveBeenCalledWith(stock);
     });
 
     it('throws an error if an invalid stock ID is provided', () => {
-        handler({ params: { id: 23 } }, res);
+        handler({ params: { id: 23 }, body: { price: 23 } }, res);
 
         expect(res.status).toHaveBeenCalledWith(404);
         expect(res.json).toHaveBeenCalledWith('Could not find any stock with ID 23');
